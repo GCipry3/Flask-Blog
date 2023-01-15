6e28e436dea2bd1e574fa8dfb9a36a1a8172deb7
GCipry3 <galbeazaciprian30@gmail.com>
2023-01-15 01:07:06 +0000

    the number of posts per page was increased

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -13,7 +13,7 @@ from flask_sqlalchemy import pagination
 @app.route('/home')
 def home():
     page = request.args.get('page', default=1, type=int)
-    posts = Post.query.order_by(Post.date_posted.desc()).paginate(per_page=4, page=page)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(per_page=6, page=page)
     return render_template('home.html', posts=posts)
 
 
